3cc987956ed3805b9abfefc0716b0c8c2bfd834b
bobby vang <fucheevang@gmail.com>
2017-11-09 20:26:59 -0800

    this is my day fuchee

--- from_fuchee/myinfo.py
+++ from_fuchee/myinfo.py
@@ -1,4 +1,61 @@
+sentence = "bobby is so cool!"
 firstname = "bobby"
+lastname = "vang"
+favoritemovies="Pokemon"
+favoriteshirt=dragonballz "shirt"
+favoritegame=dragonballz "game"
+favoritepant=short "pant"
+favoritepillow= "longpillow"
+favoritebook= "naruto"
+favoritelaptop= "laptop"
+favoritesport= "like kida all"
+favoriteball= "baskball"
+favoriteipade= "ipade 3"
+favoriteconsol= "ps4"
+favoriteanime= "dragonballz and naruto"
+favoritewallpicture "dragonballz"
+favoritesnack "hot chetto"
+favoritetvshow "pokemon"
+favoritedrick "water"
+favoritesong "from naruto"
+favoritebrother "all of them"
+favoritesister "all of them"
+carname "Acura" 
+carname "Audi"
+carname "Bentley"
+carname "BMW"
+carname "Bugatti"
+carname "Alfa Martin"
+carname "Aston Martin"
+carname "Buick"
+carname "Geely"
+carname "Jeep"
+carname "Fiatme"
+carname "TVR"
+carname "Westfield"
+carname "Sportcar"
+carname "Vale"
+carname "Ltd"
+carname "Turner"
+carname "saab"
+carname "Tesla"
+carname "Toyota"
 
+
+
+
+
+
+
+
+
+
+
+
+
+print favoritemovies
+
+
+print sentence
 print firstname
-
+print lastname
